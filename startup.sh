4584c1d0221c0d6ffa9e6a52f50485af7666d4f8
Patrick Kerwood <patrick@kerwood.dk>
2017-06-13 11:34:23 +0200

    Removed apache.pid and rtorrent.lock on startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+########## Rtorrent-LXC Container ###########
+# Author: Patrick Kerwood @ LinuxBloggen.dk #
+#############################################
+
 if [[ ! -z ${HTUSER+x} && ! -z ${HTPASS+x} ]]; then
         htpasswd -b -c /etc/apache2/.htpasswd ${HTUSER} ${HTPASS}
         a2ensite 000-default-auth.conf
@@ -10,5 +14,12 @@ if [[ ! -z ${RTORRENT_PORT+x} ]]; then
 	sed -i -e "s/51001-51001/${RTORRENT_PORT}/g" /home/rtorrent/.rtorrent.rc
 fi
 
+if [ -f /home/rtorrent/rtorrent-session/rtorrent.lock ]; then
+    rm -f /home/rtorrent/rtorrent-session/rtorrent.lock
+fi
 su rtorrent bash -c "/usr/bin/tmux -2 new-session -d -s rtorrent rtorrent"
+
+if [ -f /var/run/apache2/apache2.pid ]; then
+    rm -f /var/run/apache2/apache2.pid
+fi
 /usr/sbin/apache2ctl -D FOREGROUND
