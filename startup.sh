d86f5995fa2b4d3796f8a252b458eb1de647ae46
Patrick Kerwood <patrick@kerwood.dk>
2017-06-13 11:46:28 +0200

    Change ownership of rtorrent-session folder.

--- startup.sh
+++ startup.sh
@@ -14,12 +14,14 @@ if [[ ! -z ${RTORRENT_PORT+x} ]]; then
 	sed -i -e "s/51001-51001/${RTORRENT_PORT}/g" /home/rtorrent/.rtorrent.rc
 fi
 
+chown rtorrent.rtorrent /home/rtorrent/rtorrent-session
+
 if [ -f /home/rtorrent/rtorrent-session/rtorrent.lock ]; then
     rm -f /home/rtorrent/rtorrent-session/rtorrent.lock
 fi
 su rtorrent bash -c "/usr/bin/tmux -2 new-session -d -s rtorrent rtorrent"
 
 if [ -f /var/run/apache2/apache2.pid ]; then
-    rm -f /var/run/apache2/apache2.pid
+    rm -f /var/ru   n/apache2/apache2.pid
 fi
 /usr/sbin/apache2ctl -D FOREGROUND
